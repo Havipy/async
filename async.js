90f9197b0f054714743b900dc2a06eb4d1e5435c
Kirill <kirillmiravov@gmail.com>
2023-12-04 18:04:00 +0300

    git init

---
+++ async.js
@@ -0,0 +1,10 @@
+console.log('Start');
+console.log('Start 2');
+function timeout2sec() {
+	console.log('timeout2sec')
+}
+setTimeout(function(){
+	console.log('Inside timeout, after 2000 seconds') 
+}, 5000)
+setTimeout(timeout2sec, 2000)
+console.log('End')
